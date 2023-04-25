ee98a641326c6550a2f163bb1fd3f4c22b361614
FacundoMartinez8 <facumartinez2001@hotmail.com>
2023-04-25 09:08:09 -0300

    Committing local changes before merging with remote

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,6 @@
 import 'package:cinemapedia/core/Routes/app_routes.dart';
 import 'package:cinemapedia/core/service/dependecies_service.dart';
-import 'package:cinemapedia/features/movies/domain/usecase/get_now_playing_use_case.dart';
-import 'package:cinemapedia/features/movies/presentation/bloc/movies_bloc.dart';
-import 'package:cinemapedia/features/movies/presentation/pages/home_page.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'core/Theme/app_theme.dart';
