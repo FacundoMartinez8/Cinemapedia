d60180af8ef70506666a8d4dce2f39a36a2886b6
FacundoMartinez8 <facumartinez2001@hotmail.com>
2023-05-08 13:00:57 -0300

    ergegr

--- lib/features/movies/presentation/pages/home_page.dart
+++ lib/features/movies/presentation/pages/home_page.dart
@@ -1,8 +1,10 @@
 import 'package:cinemapedia/core/service/dependecies_services.dart';
 import 'package:cinemapedia/core/use_case/no_params.dart';
+import 'package:cinemapedia/core/widgets/custom_app_bar_widget.dart';
 import 'package:cinemapedia/core/widgets/generic_failure_widget.dart';
 import 'package:cinemapedia/core/widgets/loading_widget.dart';
 import 'package:cinemapedia/features/movies/presentation/bloc/movies_bloc.dart';
+import 'package:cinemapedia/features/movies/presentation/widgets/now_playing_movie.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -16,43 +18,9 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final _bloc = getIt<MovieBloc>();
-
-  @override
-  void initState() {
-    super.initState();
-    _bloc.add(ActionGetMovies(NoParams()));
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: BlocConsumer<MovieBloc, MovieState>(
-      bloc: _bloc,
-      listener: (context, state) {},
-      builder: (context, state) {
-        if (state is OnLoadingMovie) {
-          return const LoadingWidget();
-        } else if (state is OnLoaderMovies) {
-          final movies = state.movies;
-          return ListView.builder(
-            itemCount: movies.length,
-            itemBuilder: (BuildContext context, int index) {
-              final movie = movies[index];
-              return ListTile(
-                title: Text(movie.title),
-              );
-            },
-          );
-        } else if (state is OnFaileruMovie) {
-          return const Center(
-            child: Text('error'),
-          );
-        }
-        return Container(
-          child: Text('hola'),
-        );
-      },
-    ));
+        body: Column(children: [const CustomAppBar(), MovieSlideShows()]));
   }
 }
